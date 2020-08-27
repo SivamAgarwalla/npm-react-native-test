21149c96241b5425602a2e116af445bc0507d0e1
Sivam Agarwalla <sivam.agarwalla@gmail.com>
2020-08-26 17:50:25 -0700

    JS functions accessing Rest API

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-exports.printMsg = function () {
-  console.log('This is a message from a function in the npm package!');
+global.fetch = require('node-fetch');
+
+exports.getPosts = function () {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((data) => {
+      // Figure out how to return data from promises so it can be rendered in Expo
+      console.log('POSTS');
+      data.forEach((post) => console.log(post.title));
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getPostComments = function () {
+  fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((comment) => console.log(comment.email));
+    })
+    .catch((err) => console.log(err));
+};
+
+// Created a config file for this function -> addPostConfig.json
+exports.addPost = function () {
+  let configjson = require('/Users/sivamagarwalla/Desktop/npm-react-native-test/addPostConfig.json');
+
+  fetch(configjson.BASE_URL, {
+    method: configjson.REQUEST_TYPE,
+    headers: {
+      Accept: configjson.REQUEST_HEADER.Accept,
+      'Content-type': configjson.REQUEST_HEADER['Content-type'],
+    },
+    body: JSON.stringify({
+      title: configjson.REQUEST_PARAMETERS.title,
+      body: configjson.REQUEST_PARAMETERS.body,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
 };
